337d376810dcbbc75a871303d40c809a53afbc15
Ha Nguyen <nguyenthingocha2@gmail.com>
2020-02-11 23:19:37 -0500

    fixing jumbotron on html file, adding pic, fixing java script files for quiz and code, add timer, add score tracker

--- pop-quiz.js
+++ pop-quiz.js
@@ -8,10 +8,33 @@ var score = 0;
 var secondsLeft = 75;
 var currentQuestionIndex = 0;
 
+
+
+
+
+
+var timerInterval;
+
+function timer() {
+  if (secondsLeft > 9) {
+    secondsLeft--;
+    countdownPrintVariable.textContent = "0:" + secondsLeft;
+  } else if (secondsLeft > 0) {
+    secondsLeft--;
+    countdownPrintVariable.textContent = "0:0" + secondsLeft;
+  } else {
+    // you lose the game
+    clearInterval(timerInterval);
+    messagePrintVariable.textContent = "you lose!";
+  }
+}
+
+
+
 function goToNextQuestion(whatTheUserClicked) {
     var correctText = questions[currentQuestionIndex].answer;
 
-    if (whatTheUserClicked === correctText) {
+    if (whatTheUserClicked == correctText) {
         console.log("Correct!");
         score++;
     }
@@ -24,21 +47,29 @@ function goToNextQuestion(whatTheUserClicked) {
 
 function answerClickSetup() {
     var a = document.getElementById("answer1");
-    console.log(a);
+    //console.log(a);
+    var b = document.getElementById("answer2");
+    var c = document.getElementById("answer3");
+    var d = document.getElementById("answer4");
 
 a.addEventListener("click", function () { goToNextQuestion(a.innerText); });
+b.addEventListener("click", function () { goToNextQuestion(b.innerText); });
+c.addEventListener("click", function () { goToNextQuestion(c.innerText); });
+d.addEventListener("click", function () { goToNextQuestion(d.innerText); });
 }
 
 answerClickSetup();
 
 startQuiz.addEventListener("click", function () {
+
+    timerInterval = setInterval(timer, 1000);
     getNewQuestion(currentQuestionIndex);
 });
 
 var currentQuestion;
 function getNewQuestion(questionIndex) {
     var question = questions[questionIndex];
-    console.log(question);
+    //console.log(question);
     currentQuestion = question;
     var title = question.title;
     //console.log(title);
@@ -47,12 +78,37 @@ function getNewQuestion(questionIndex) {
 
     var choice1 = question.choices[0];
     var answerEl1 = document.getElementById("answer1");
-    console.log(answerEl1);
+    //console.log(answerEl1);
     answerEl1.innerText = choice1;
     
-    var choice2 = question.choices[1];
+    var choice2 = question.choices[3];
     var answerEl2 = document.getElementById("answer2");
-    console.log(answerEl2);
-    answerEl1.innerText = choice2;
-}
+    //console.log(answerEl2);
+    answerEl2.innerText = choice2;
+    //console.log(choice2);
 
+    var choice3 = question.choices[2];
+    var answerEl3 = document.getElementById("answer3");
+    //console.log(answerEl3);
+    answerEl3.innerText = choice3;
+    //console.log(choice3);
+
+    var choice4 = question.choices[0];
+    var answerEl4 = document.getElementById("answer4");
+    //console.log(answerEl4);
+    answerEl4.innerText = choice4;
+    //console.log(choice4);
+
+    document.getElementById("question").innerText = title;
+    document.getElementById("answer1").innerText = choice1;
+    document.getElementById("answer2").innerText = choice2;
+    document.getElementById("answer3").innerText = choice3;
+    document.getElementById("answer4").innerText = choice4;
+
+}
+//submitAnswer.addEventListener("click", quizTime);
+function scoreKeeper(){
+    document.getElementById("score").innerHTML = score++;
+  
+  scoreKeeper();
+}
--- question.js
+++ question.js
@@ -1,15 +1,32 @@
 var questions = [
     {
-        title: "Who is the main character in CYE",
+        title: "Who is the main character in Curb Your Enthusiasm?",
         choices: ["Larry David", "Cheryl David", "Leon Black", "Jeff Green"],
         answer: "Larry David"
     },
 
     {
         title: "Why doesn’t Larry want to go back in and get his watch after he and Cheryl leave a dinner party?",
-        choices: ["Double-dip", "Double-goodbye", "Double-drive", "double-dance"],
+        choices: ["Double-dip", "Double-goodbye", "Double-drive", "Double-dance"],
         answer: "Double-goodbye"
     },
 
+    {
+        title: "Before “Curb Your Enthusiasm”, what show did Larry David write?",
+        choices: ["Friends", "Arrested Development", "Sex and the City", "Seinfeld"],
+        answer: "Seinfeld"
+    },
+
+    {
+        title: "What is the name of Larry’s manager?",
+        choices: ["Leon Black", "Ted Danson", "Jeff Green", "Cheryl David"],
+        answer: "Jeff Green"
+    },
+    
+    {
+        title: "Who is Larry’s best friend?",
+        choices: ["Leon Black", "Marty Funckhouser", "Jeff Green", "Richard Lewis"],
+        answer: "Leon Black"
+    },
 
 ]
